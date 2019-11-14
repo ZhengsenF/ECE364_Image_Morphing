cfd1a7301357117d01b1960748f40bee74de8ae5
ZhengsenF <42655384+ZhengsenF@users.noreply.github.com>
2019-11-14 02:15:58 -0500

    Finished loadTriangles

--- Morphing.py
+++ Morphing.py
@@ -8,15 +8,29 @@ import numpy as np
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
 
+
 # This function takes in the full file paths of the text files containing the (x, y)
 # coordinates of a list of points, for both the left and right images.
 # returns the tuple (leftTriangles, rightTriangles), where each is a list of instances of the Triangle class.
 def loadTriangles(leftPointFilePath, rightPointFilePath):
     leftPoints = pointsFromFile(leftPointFilePath)
     rightPoints = pointsFromFile(rightPointFilePath)
-    leftTri = Delaunay(leftPoints)
-    print(leftTri.simplices)
-    return (1, 2)
+    leftDelaunay = Delaunay(leftPoints)
+    rightDelaunay = Delaunay(rightPoints)
+    leftTriangles = triangleFromDelaunay(leftDelaunay, leftPoints)
+    rightTriangles = triangleFromDelaunay(rightDelaunay, rightPoints)
+    return tuple([leftTriangles, rightTriangles])
+
+
+# This function generates a list of triangle object from Delaunay object
+# returns a list of triangles
+def triangleFromDelaunay(delaunyObject, points):
+    triangles = []
+    for each in delaunyObject.simplices:
+        newTri = Triangle(np.array([points[each[0]], points[each[1]], points[each[2]]]))
+        triangles.append(newTri)
+        print(newTri)
+    return triangles
 
 
 # This function read points from file
@@ -38,6 +52,10 @@ class Triangle:
         if vertices.dtype is not np.dtype('float64'):
             raise ValueError('Vertices of the triangle should have a type of float64')
         self.vertices = vertices
+
+    # For debug purpose
+    def __str__(self):
+        return str(self.vertices)
 
     def getPoints(self):
         pass
